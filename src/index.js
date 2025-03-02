f412c9c23546a9c31f7de6ddb6077797e00c5025
Luis Urpay <85956643+luisurpay7@users.noreply.github.com>
2025-03-01 21:42:21 -0500

    Update index 2.js

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 // Definir un endpoint GET /hello
 app.get('/hello', (req, res) => {
-    res.send('Hello, World 3!');
+    res.send('Hello, World 4!');
 });
 
 // Iniciar el servidor
